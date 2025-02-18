5b47c6b43c6385f2d47317d36bd83f6b553cbcd3
Gaelpem <pembelegael@gmail.com>
2025-02-18 17:02:51 +0100

    commencement

--- index.php
+++ index.php
@@ -3,22 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Oswald:wght@200..700&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Oswald:wght@200..700&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="asset/css/style.css">
     <title>Document</title>
 </head>
 <body>
     <header>
+        <div class="personal1">
+            <p>photographer /</p> 
+            <p>[name] : gaël pembele</p>
+        </div>
+        <div class="heure">
+            <p id="heure">12:33:03 pm,</p>
+        </div>
         <nav>
             <ul class="links">
-                
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-
-
+                <li><a href="">about——</a></li>
+                <li><a href="">works</a></li>
             </ul>
         </nav>
     </header>
     
+    <main>
+        <section class="about">
+
+        </section>
+
+
+
+
+    </main>
 
 
     <script src="asset/js/app.js"></script>
