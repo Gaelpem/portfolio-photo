d6a0e2737e5b449876bba5b46da336b166a716c5
Gaelpem <pembelegael@gmail.com>
2025-02-19 08:30:05 +0100

    style

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Oswald:wght@200..700&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Oswald:wght@200..700&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="asset/css/style.css">
     <title>Document</title>
 </head>
@@ -29,7 +31,15 @@
     
     <main>
         <section class="about">
-
+            <div class="entete">
+                <h1 class="titre">———about</span></h1>
+                 <div class="number-fleche">
+                    <p>[1]</p>
+                 <i id= "arrow"class="bi bi-arrow-down-circle-fill"></i>
+                 </div>
+            </div>
+            <p class="paragraphe">Passionate about photography since —2020, I started by exploring street[—]photography with a film camera, which allowed me to develop and refine my artistic vision. For fashion / photography, I use a digital [—]camera to capture sharper and more dynamic images——————.</p>
+            <p class=>©2020</p>
         </section>
 
 
